703506ec8594e5a2ea77ea2b26591ed15ce0802e
Gise Haag <ing.giselahaag@gmail.com>
2021-03-12 12:30:37 +0100

    Add HTML response boby

--- endpoints/search.php
+++ endpoints/search.php
@@ -11,12 +11,31 @@ if( !isset($query['query']) || empty(trim($query['query']))) {
 
 $default_query = array(
 	'per_page'		=> 8,
+	'order_by'		=> 'lastest',
 );
 
 $merged_query = array_merge( $default_query, $query);
-
 $search = $unsplash->fetch('/search/photos', $merged_query);
+$results = json_decode($search)->results;
 
-echo $search;
+foreach ($results as $item) : ?>
+	<div class="image">
+		<img
+			src="<?php echo $item->urls->small; ?>"
+			alt="<?php echo $item->alt_description; ?>"
+		/>
+		<span class="caption"><?php echo $item->alt_description; ?></span>
+		<div class="author-data">
+			<a href="">
+				<img
+					class="author-image"
+					src="<?php echo $item->user->profile_image->small; ?>"
+					alt="<?php echo $item->user->name; ?>"
+				/>
+			</a>
+			<span class="author-name"><?php echo $item->user->name; ?></span>
+		</div>
+	</div>
+<?php endforeach;
 
 
