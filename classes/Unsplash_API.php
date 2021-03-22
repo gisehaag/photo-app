0459e1b28c6bba0cdb5cb27684e602f8f51fe264
Gise Haag <ing.giselahaag@gmail.com>
2021-03-22 19:11:42 +0100

    Improve cache data logic

--- classes/Unsplash_API.php
+++ classes/Unsplash_API.php
@@ -13,13 +13,31 @@ class Unsplash_API {
 		$this->base_path = $dir;
 	}
 
+	private function save_to_file($endpoint, $decode_response, $params) {
+		$file_name = $this->get_file_name($endpoint, $params);
+		file_put_contents( $file_name, json_encode($decode_response));
+	}
+
+	private function get_file_name($endpoint, $params = []) {
+		$file_name = str_replace('/','-',$endpoint);
+		$file_name = substr($file_name,1);
+
+		if(strpos($file_name, 'search') !== false) {
+			$query_term = isset($params['query'])? $params['query'] : 'default';
+			$page = isset($params['page']) ? $params['page'] : 1;
+			$file_name .= '-' . $query_term . '-' . $page;
+		}
+
+ 		return $this->base_path . '/data/' . $file_name .'.json';
+	}
+
 	private function fetch($endpoint, $params = [])	{
 		$default_params = array(
 			'client_id' 	=> $this->client_id,
 			'per_page'		=> 21,
 		);
 
-		$curl_params = http_build_query( array_merge( $default_params, $params));
+		$curl_params = http_build_query( array_merge( $default_params, $params) );
 
 		$curl = curl_init();
 
@@ -50,16 +68,16 @@ class Unsplash_API {
 			if (is_null($decode_response)) {
 			  throw ($response);
 			}
-		 } catch (Exception $e) {
+		} catch (Exception $e) {
 			return json_decode(
 				"{
 					'error': true,
 					'message': 'Caught exception: {$e->getMessage()}'
 				}"
 			);
-		 }
+		}
 
-		file_put_contents($this->base_path . '/data/' . str_replace('/','-',$endpoint) . '.json', json_encode($decode_response));
+		$this->save_to_file($endpoint, $decode_response, $params);
 		return $decode_response;
 	}
 
@@ -68,14 +86,53 @@ class Unsplash_API {
 			return false;
 		}
 
-		$file = $this->base_path . '/data/' . str_replace('/','-',$endpoint) . '.json';
+		$file_path = $this->get_file_name($endpoint, $params);
+		$this->delete_if_old_file($file_path);
 
-
-		if( file_exists($file) ) {
-			return json_decode(file_get_contents($file));
+		if( file_exists($file_path) ) {
+			return $this->get_file_content($file_path);
 		}
 
-		//Si llegue a este punto
 		return $this->fetch($endpoint, $params);
 	}
+
+	private function get_file_content($file) {
+		return json_decode(file_get_contents($file));
+	}
+
+	public function get_file_age($file) {
+		$today = getdate(time())[0];
+
+		if( isset( $file ) ) {
+			$date_creation = filemtime( $file );
+			$interval_days = ( $today - $date_creation ) / 3600 / 24;
+			return $interval_days;
+		}
+	}
+
+	public function delete_if_old_file( $file ) {
+		$DEFAULT_MAX_AGE = 30;
+
+		$max_age_per_file = array(
+			'topics-'		=> 7,
+			'users-'			=> 7,
+			'search-'		=> 1,
+		);
+
+		if( file_exists($file) ) {
+			$file_age = $this->get_file_age($file);
+
+			foreach($max_age_per_file as $name => $limit_age) {
+				if(strpos($file, $name) !== false) {
+					if($file_age > $limit_age){
+						unlink($file);
+					};
+				} else if($file_age > $DEFAULT_MAX_AGE){
+					unlink($file);
+				}
+			}
+		}
+	}
+
+
 }
